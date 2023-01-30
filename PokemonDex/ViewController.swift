3fbda68cd7270aabc092e052fcad2af3fb7d598d
Chris Manik <chris.manikjr@gmail.com>
2023-01-30 22:14:56 +0700

    Initial Commit

---
+++ PokemonDex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonDex
+//
+//  Created by Chrismanto Natanail Manik on 30/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
